d70603324be81524627d62ba7de67ff940b8e360
smv159 <smv159@psu.edu>
2023-02-09 16:51:57 -0500

    New Test File

---
+++ src/IST261/CoffeeTestHarness.java
@@ -0,0 +1,16 @@
+package IST261;
+
+public class CoffeeTestHarness {
+    static public void testCoffee() {
+        System.out.println("Coffee");
+    }
+
+    static public void testCurrent() {
+        System.out.println("Current");
+    }
+
+    public static void main(String[] args) {
+        CoffeeTestHarness.testCoffee();
+        CoffeeTestHarness.testCurrent();
+    }
+}
